cba22de0f6b08e442976cbf319c1ff91f4dd97b9
Dave Smyth <websmyth@users.noreply.github.com>
2021-09-24 21:35:31 +0100

    Initial fork

---
+++ sites.js
@@ -0,0 +1,10 @@
+let sites = [
+    {
+        name: "Matt Webb",
+        url: 'https://interconnected.org/home/2020/09/24/unoffice_hours'
+    },
+    {
+        name: "Dave Smyth",
+        url: 'https://davesmyth.com/unoffice-hours'
+    }
+]
