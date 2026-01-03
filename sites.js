6e49ca17a41543d4f5c07be09909d1790856626e
Matt Sephton <matt.sephton@gmail.com>
2026-01-03 22:20:20 +0000

    Adds Matt Sephton @gingerbeardman
    and tweak formatting

--- sites.js
+++ sites.js
@@ -100,7 +100,11 @@ let sites = [
         url: "https://blog.greg.technology/2025/08/30/chat.html"
     },
     {
-    name: "Javier Arce",
-    url: "https://javier.computer/unoffice-hours"
-  }
+        name: "Javier Arce",
+        url: "https://javier.computer/unoffice-hours"
+    },
+    {
+        name: "Matt Sephton",
+        url: "https://blog.gingerbeardman.com/unoffice-hours"
+    },
 ]
